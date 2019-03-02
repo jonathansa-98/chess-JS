f2a26ca8bad1b26e59ac443859ed76ce2b8042ad
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-03-02 13:45:00 +0100

    pensando como mover fichas

--- js/Piezas.js
+++ js/Piezas.js
@@ -1,5 +1,100 @@
 function Pieza(id, img, equipo) {
     this.id = id || "";
     this.img = img || ""; // imagen de la pieza en el tablero.
-    this.equipo = equipo || 0; // 0 == blancas && 1 == negras.
+    this.equipo = equipo || 0; // -1 == blancas && 1 == negras && 0 vacio.
 }
+
+Pieza.prototype.print = function () {
+    return `ID: ${this.id} Img: ${this.img} Equipo: ${this.equipo}`;
+}
+
+/** Peon */
+Peon.prototype = new Pieza();
+Peon.prototype.constructor = Peon;
+
+function Peon(id, img, equipo) {
+    this.base = Pieza;
+    this.base(id, img, equipo);
+}
+
+Peon.prototype.mover = function(x, y) {
+    if(x<8 && y<8){
+        return "2 primer turno, luego 1 delante o ataca diagonal";
+    }
+};
+/******************* */
+/** Torre */
+Torre.prototype = new Pieza();
+Torre.prototype.constructor = Torre;
+
+function Torre(id, img, equipo) {
+    this.base = Pieza;
+    this.base(id, img, equipo);
+}
+
+Torre.prototype.mover = function(x, y) {
+    if(x<8 && y<8){
+        return "infinito recto";
+    }
+};
+/******************* */
+/** Caballo */
+Caballo.prototype = new Pieza();
+Caballo.prototype.constructor = Caballo;
+
+function Caballo(id, img, equipo) {
+    this.base = Pieza;
+    this.base(id, img, equipo);
+}
+
+Caballo.prototype.mover = function(x, y) {
+    if(x<8 && y<8){
+        return "en L";
+    }
+};
+/******************* */
+/** Alfil */
+Alfil.prototype = new Pieza();
+Alfil.prototype.constructor = Alfil;
+
+function Alfil(id, img, equipo) {
+    this.base = Pieza;
+    this.base(id, img, equipo);
+}
+
+Alfil.prototype.mover = function(x, y) {
+    if(x<8 && y<8){
+        return "en diagonal";
+    }
+};
+/******************* */
+/** Dama */
+Dama.prototype = new Pieza();
+Dama.prototype.constructor = Dama;
+
+function Dama(id, img, equipo) {
+    this.base = Pieza;
+    this.base(id, img, equipo);
+}
+
+Dama.prototype.mover = function(x, y) {
+    if(x<8 && y<8){
+        return "infinito en diagonal, vertical y horizontal";
+    }
+};
+/******************* */
+/** Rey */
+Rey.prototype = new Pieza();
+Rey.prototype.constructor = Rey;
+
+function Rey(id, img, equipo) {
+    this.base = Pieza;
+    this.base(id, img, equipo);
+}
+
+Rey.prototype.mover = function(x, y) {
+    if(x<8 && y<8){
+        return "cualquier alrededor suyo 1 celda";
+    }
+};
+/******************* */
--- js/chess.js
+++ js/chess.js
@@ -1,8 +1,12 @@
 /**** VARIABLES ****/
 const NUM_CELDAS = 8;
 const TABLERO = [];
-const BLANCAS = [];
-const NEGRAS = [];
+const PIEZAS_BLANCAS = [];
+const PIEZAS_NEGRAS = [];
+const TEAM_BLANCAS = -1;
+const TEAM_NEGRAS = 1;
+
+let celdas;
 
 /****************** */
 /** preparacion inicial */
@@ -24,31 +28,31 @@ function pintaTablero() {
 }
 
 function creaNegras() {
-    NEGRAS.push(new Pieza("0-0", "/img/NT.png", "negra"));
-    NEGRAS.push(new Pieza("0-1", "/img/NC.png", "negra"));
-    NEGRAS.push(new Pieza("0-2", "/img/NA.png", "negra"));
-    NEGRAS.push(new Pieza("0-3", "/img/ND.png", "negra"));
-    NEGRAS.push(new Pieza("0-4", "/img/NR.png", "negra"));
-    NEGRAS.push(new Pieza("0-5", "/img/NA.png", "negra"));
-    NEGRAS.push(new Pieza("0-6", "/img/NC.png", "negra"));
-    NEGRAS.push(new Pieza("0-7", "/img/NT.png", "negra"));
+    PIEZAS_NEGRAS.push(new Torre("0-0", "img/NT.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Caballo("0-1", "img/NC.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Alfil("0-2", "img/NA.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Dama("0-3", "img/ND.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Re("0-4", "img/NR.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Pieza("0-5", "img/NA.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Pieza("0-6", "img/NC.png", TEAM_NEGRAS));
+    PIEZAS_NEGRAS.push(new Pieza("0-7", "img/NT.png", TEAM_NEGRAS));
     for (let i = 0; i < NUM_CELDAS; i++) {
-        NEGRAS.push(new Pieza("1-"+i, "/img/NP.png", "negra"));
+        PIEZAS_NEGRAS.push(new Pieza("1-"+i, "img/NP.png", TEAM_NEGRAS));
     }
 }
 
 function creaBlancas() {
     for (let i = 0; i < NUM_CELDAS; i++) {
-        BLANCAS.push(new Pieza("7-" + i, "/img/BP.png", "blanca"));
+        PIEZAS_BLANCAS.push(new Pieza("7-" + i, "img/BP.png", TEAM_BLANCAS));
     }
-    BLANCAS.push(new Pieza("8-0", "/img/BT.png", "blanca"));
-    BLANCAS.push(new Pieza("8-1", "/img/BC.png", "blanca"));
-    BLANCAS.push(new Pieza("8-2", "/img/BA.png", "blanca"));
-    BLANCAS.push(new Pieza("8-3", "/img/BD.png", "blanca"));
-    BLANCAS.push(new Pieza("8-4", "/img/BR.png", "blanca"));
-    BLANCAS.push(new Pieza("8-5", "/img/BA.png", "blanca"));
-    BLANCAS.push(new Pieza("8-6", "/img/BC.png", "blanca"));
-    BLANCAS.push(new Pieza("8-7", "/img/BT.png", "blanca"));
+    PIEZAS_BLANCAS.push(new Pieza("8-0", "img/BT.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-1", "img/BC.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-2", "img/BA.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-3", "img/BD.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-4", "img/BR.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-5", "img/BA.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-6", "img/BC.png", TEAM_BLANCAS));
+    PIEZAS_BLANCAS.push(new Pieza("8-7", "img/BT.png", TEAM_BLANCAS));
 }
 
 function pintaCelda(x, y) {
@@ -64,7 +68,39 @@ function pintaCelda(x, y) {
 }
 
 function pintaPiezas() {
-
+    celdas = document.querySelectorAll("#tablero > div");
+    // espacio negras
+    for (let n = 0; n < PIEZAS_NEGRAS.length; n++) {
+        TABLERO.push(PIEZAS_NEGRAS[n]);
+    }
+    // espacio vacio
+    for (let i = 0; i < NUM_CELDAS/2; i++) {
+        for (let vacio = 0; vacio < NUM_CELDAS; vacio++) {
+            TABLERO.push(new Pieza());
+        }
+    }
+    // espacio blancas
+    for (let b = 0; b < PIEZAS_BLANCAS.length; b++) {
+        TABLERO.push(PIEZAS_BLANCAS[b]);
+    }
+    // pinta tablero entero
+    pintaUI();
+    console.log(TABLERO);
+    // movimiento de prueba
+    setTimeout(() => {
+        mueveCelda(Math.floor(Math.random() * 64), Math.floor(Math.random() * 64));
+        pintaUI();
+        console.log(TABLERO);
+    }, 1000);
 }
 
+function mueveCelda(n1, n2) {
+    [TABLERO[n1], TABLERO[n2]] = [TABLERO[n2], TABLERO[n1]];
+}
+
+function pintaUI() {
+    for (let c = 0; c < celdas.length; c++) {
+        celdas[c].style.backgroundImage = `url('${TABLERO[c].img}')`;
+    }
+}
 /**************************************************** */
