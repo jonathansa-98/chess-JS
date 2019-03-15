83b3a4ef1c1fbaa6dcdd6e18763345acb05cb996
Jonathan <jonathansa98@gmail.com>
2019-03-15 19:46:27 +0100

    añadidos movimientos de peones

--- js/Piezas.js
+++ js/Piezas.js
@@ -36,27 +36,71 @@ function Peon(id, img, equipo, move) {
     this.move = move || 0;
 }
 
+Peon.prototype.calcAtaquesPosibles = function(pos, posibles) {
+    var posicion = pos.slice();
+    if(this.equipo == TEAM_NEGRAS) {
+        try{
+            posicion[0] += 1;
+            posicion[1] -= 1;
+            if (this.calcCollision(posicion) == -1) {
+                posibles.push(posicion.slice(0));
+            }
+        } catch(e) {}
+        posicion = pos.slice();
+        try {
+            posicion[0] += 1;
+            posicion[1] += 1;
+            if (this.calcCollision(posicion) == -1) {
+                posibles.push(posicion.slice(0));
+            }
+        } catch (e) {}
+    } else if (this.equipo == TEAM_BLANCAS) {
+        try {
+            posicion[0] -= 1;
+            posicion[1] -= 1;
+            if (this.calcCollision(posicion) == -1) {
+                posibles.push(posicion.slice(0));
+            }
+        } catch (e) {}
+        posicion = pos.slice();
+        try {
+            posicion[0] -= 1;
+            posicion[1] += 1;
+            if (this.calcCollision(posicion) == -1) {
+                posibles.push(posicion.slice(0));
+            }
+        } catch (e) {}
+    }
+};
+
 // devuelve un array con las coordenadas de cada posicion posible
 Peon.prototype.getMovPosibles = function() {
     var pos = [];
     var posibles = [];
+    pos = this.id.split("-");
+    pos = pos.map(Number);
+    this.calcAtaquesPosibles(pos, posibles);
     if (this.equipo == TEAM_NEGRAS) {
-        pos = this.id.split("-");
-        pos = pos.map(Number);
         pos[0] += 1;
-        posibles.push(pos.slice(0));
-        if(this.move < 1) {
-            pos[0] +=1;
+        if (this.calcCollision(pos) == 0){
             posibles.push(pos.slice(0));
+            if(this.move < 1) {
+                pos[0] +=1;
+                if (this.calcCollision(pos) == 0) {
+                    posibles.push(pos.slice(0));
+                }
+            }
         }
     } else if (this.equipo == TEAM_BLANCAS) {
-        pos = this.id.split("-");
-        pos = pos.map(Number);
         pos[0] -= 1;
-        posibles.push(pos.slice(0));
-        if (this.move < 1) {
-            pos[0] -= 1;
+        if (this.calcCollision(pos) == 0) {
             posibles.push(pos.slice(0));
+            if (this.move < 1) {
+                pos[0] -= 1;
+                if (this.calcCollision(pos) == 0) {
+                    posibles.push(pos.slice(0));
+                }
+            }
         }
     }
     //console.log(posibles);
@@ -114,7 +158,7 @@ Torre.prototype.getMovPosibles = function () {
     }
     //console.log(posibles);
     return posibles;
-}
+};
 
 /******************* */
 /** Caballo */
@@ -186,7 +230,7 @@ Caballo.prototype.getMovPosibles = function () {
     }
     //console.log(posibles);
     return posibles;
-}
+};
 /******************* */
 /** Alfil */
 Alfil.prototype = new Pieza();
@@ -242,7 +286,7 @@ Alfil.prototype.getMovPosibles = function () {
     }
     //console.log(posibles);
     return posibles;
-}
+};
 /******************* */
 /** Dama */
 Dama.prototype = new Pieza();
@@ -332,7 +376,7 @@ Dama.prototype.getMovPosibles = function () {
     }
     //console.log(posibles);
     return posibles;
-}
+};
 /******************* */
 /** Rey */
 Rey.prototype = new Pieza();
@@ -399,5 +443,5 @@ Rey.prototype.getMovPosibles = function () {
     }
     //console.log(posibles);
     return posibles;
-}
+};
 /******************* */
